a1392256ec567887b4bfeca75f8e9145958b5adc
Hanumanthareddy <reddyreddy884@gmail.com>
2024-01-23 23:50:03 +0530

    model are created and migrate

--- base/views.py
+++ base/views.py
@@ -1,24 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
-rooms =[
-    {'id':1, 'name':'Lets learn python'},
-    {'id':2, 'name':'Laravel Developer'},
-    {'id':3 , 'name':'Django Developer'},
-]
+from .models import Room
 
 # Create your views here.
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms':rooms}
     return render(request,'base/home.html',context)
 
 def room(request,pk=None):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
-            break
+    room = Room.objects.get(id=pk)
     context = {'room':room}
-
     return render(request,'base/room.html', context)
