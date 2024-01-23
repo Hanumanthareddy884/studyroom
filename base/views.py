081209f0eac76b2ce3cf897e92b828839ff1f393
Hanumanthareddy <reddyreddy884@gmail.com>
2024-01-23 12:10:51 +0530

    Jinja  templates and model created

--- base/views.py
+++ base/views.py
@@ -2,11 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-
+rooms =[
+    {'id':1, 'name':'Lets learn python'},
+    {'id':2, 'name':'Laravel Developer'},
+    {'id':3 , 'name':'Django Developer'},
+]
 
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    context = {'rooms':rooms}
+    return render(request,'base/home.html',context)
 
-def room(request):
-    return render(request,'room.html')
+def room(request,pk=None):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+            break
+    context = {'room':room}
+
+    return render(request,'base/room.html', context)
--- studyroom/settings.py
+++ studyroom/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'studyroom.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'study_room',
+        'USER': 'root',
+        'PASSWORD': 'admin@123',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
