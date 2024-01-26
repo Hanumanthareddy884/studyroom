de2cd87cee8bfe227593d1670da35946f36f14e1
Hanumanthareddy <reddyreddy884@gmail.com>
2024-01-26 22:20:46 +0530

    Login and Logout is complited

--- base/views.py
+++ base/views.py
@@ -1,11 +1,47 @@
 from django.shortcuts import render,redirect
-from .models import Room
+from django.db.models import Q #use to query of multiple
+from django.contrib.auth.models import User# inbuilt user access
+from django.contrib import messages # uses for flass message
+from django.contrib.auth import authenticate,login,logout # inbult login,logout and authenticate
+from .models import Room,Topic
 from .forms import RoomForm
 
 # Create your views here.
+def loginPage(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request,"User does not exist")
+
+        user = authenticate(request,username=username,password=password)
+
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request,"Username or Password is Invalid")
+
+    context = {}
+    return render(request,"base/login_register.html",context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect("login")
+
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms':rooms}
+    q= request.GET.get('q') if request.GET.get('q') !=None else ''
+
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q)|
+        Q(name__icontains = q)|
+        Q(description__icontains=q)
+        )
+    topics = Topic.objects.all()
+    context = {'rooms':rooms,'topics':topics}
     return render(request,'base/home.html',context)
 
 def room(request,pk=None):
