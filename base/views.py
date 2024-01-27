3ef50c53818fc12fbceb1464d4c5410a76ccbc0b
Hanumanthareddy <reddyreddy884@gmail.com>
2024-01-27 22:07:26 +0530

    Authentication is complited

--- base/views.py
+++ base/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render,redirect
 from django.db.models import Q #use to query of multiple
-from django.contrib.auth.models import User# inbuilt user access
+from django.contrib.auth.models import User # inbuilt user access
+from django.contrib.auth.decorators import login_required #add perticular user
+from django.http import HttpResponse
 from django.contrib import messages # uses for flass message
 from django.contrib.auth import authenticate,login,logout # inbult login,logout and authenticate
+from django.contrib.auth.forms import UserCreationForm
 from .models import Room,Topic
 from .forms import RoomForm
 
 # Create your views here.
 def loginPage(request):
+    page = 'login'
+    if request.user.is_authenticated: # If is already login is redirect to Home page
+        return redirect('home')
     if request.method == "POST":
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
 
         try:
@@ -25,12 +31,27 @@ def loginPage(request):
         else:
             messages.error(request,"Username or Password is Invalid")
 
-    context = {}
+    context = {'page':page}
     return render(request,"base/login_register.html",context)
 
 def logoutUser(request):
     logout(request)
     return redirect("login")
+
+def registerPage(request):
+    form = UserCreationForm()
+    if request.method=='POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False) # commit false is data is not inserted DB
+            user.username = user.username.lower()
+            user.save()
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request,"Error is occure during registration")
+
+    return render(request,'base/login_register.html',{"form":form})
 
 def home(request):
     q= request.GET.get('q') if request.GET.get('q') !=None else ''
@@ -49,7 +70,9 @@ def room(request,pk=None):
     context = {'room':room}
     return render(request,'base/room.html', context)
 
+@login_required(login_url='login')
 def room_form(request):
+
     form = RoomForm() # model form
     if request.method == 'POST':
         form = RoomForm(request.POST)
@@ -60,8 +83,11 @@ def room_form(request):
     context = {'form':form}
     return render(request,'base/room_form.html',context)
 
+@login_required(login_url='login')
 def updateRoom(request,pk):
     room = Room.objects.get(id=pk)
+    if request.user != room.host:
+        return HttpResponse("Your not access")
     form =RoomForm(instance=room)
     if request.method == 'POST':
         form = RoomForm(request.POST,instance=room)
